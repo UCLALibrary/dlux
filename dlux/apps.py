f6b27bc2ba6f1395dca8b20cafe80fc0e2b72ca0
Andy Kohler <akohler@library.ucla.edu>
2026-04-06 14:11:05 -0800

    Update README.md with basic info for the new application

--- dlux/apps.py
+++ dlux/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MyAppNameConfig(AppConfig):
+class DluxConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'my_app_name'
+    name = 'dlux'
--- project/settings.py
+++ project/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_bootstrap5",
-    "my_app_name",
+    "dlux",
 ]
 
 MIDDLEWARE = [
